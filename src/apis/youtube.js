ceb8e8930b120c8fd8bcfe41d1f5eddc21512ef1
Vineet <vineet981013@gmail.com>
2020-03-05 17:04:00 +0530

    project youtube

---
+++ src/apis/youtube.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+//const KEY = "AIzaSyBXAyimeyDQaq0xckMpjEG4NzYGJR_XTbg";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3"
+  //   params: {
+
+  //   }
+});
