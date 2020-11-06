06202627ebd49c4f94dfe31fa354429a10faad91
Vineet <52093528+vineetpjp@users.noreply.github.com>
2020-11-06 16:29:42 +0530

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-//const KEY = "AIzaSyBXAyimeyDQaq0xckMpjEG4NzYGJR_XTbg";
-
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3"
   //   params: {
